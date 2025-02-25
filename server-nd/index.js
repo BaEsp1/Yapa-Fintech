330289c508b88458ef7cf579c0e3d8f6e2fca36b
Bárbara Espinola <Baesp2023@gmail.com>
2025-02-25 20:29:05 -0300

    update swagger

--- server-nd/index.js
+++ server-nd/index.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json());
 app.use('/api', routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 (async () => {
   try {
