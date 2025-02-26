60eaf269d9b00276c420af3cabbd64e8dfd8386d
Bárbara Espinola <Baesp2023@gmail.com>
2025-02-26 17:20:04 -0300

    Update index.js

--- server-nd/index.js
+++ server-nd/index.js
@@ -9,7 +9,7 @@ const swaggerJsDoc = require("swagger-jsdoc");
 
 const PORT = process.env.PORT || 4000;
 const URL_BACK = "https://yapa-fintech-back.vercel.app" 
-const CSS_URL ="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+const CSS_URL ="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
 
 const app = express();
 
@@ -27,7 +27,7 @@ const options = {
     },
     servers: [
       {
-        url: `${URL_BACK}/api`, // URL completa del backend
+        url: `${URL_BACK}/api`, 
       },
     ],
     components: {
@@ -40,7 +40,7 @@ const options = {
       },
     },
   },
-  apis: [`${__dirname}/routes/*.js`, `${__dirname}/routes/**/*.js`], // 🔥 Usa rutas absolutas
+  apis: [`${__dirname}/routes/*.js`, `${__dirname}/routes/**/*.js`],
 };
 
 
@@ -49,7 +49,11 @@ const swaggerSpec = swaggerJsDoc(options);
 app.use(
   "/api/docs",
   swaggerUI.serve,
-  swaggerUI.setup(swaggerSpec, { customCssUrl: CSS_URL })
+  swaggerUI.setup(swaggerSpec, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+  }),
 );
 
 app.use("/api", routes);
