76ee32c4525a5b931858e9a44a49a9dcf3871a1f
Bárbara Espinola <Baesp2023@gmail.com>
2025-02-26 16:21:43 -0300

    test3

--- server-nd/index.js
+++ server-nd/index.js
@@ -1,40 +1,69 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { conn } = require('./dataBase/dataBase');
-const routes= require('./routes/index')
-const morgan = require('morgan')
-const {swaggerUi, swaggerSpec } = require('./swaggerConfig');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { conn } = require("./dataBase/dataBase");
+const routes = require("./routes/index");
+const morgan = require("morgan");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
 const PORT = process.env.PORT || 4000;
+const URL_BACK = "https://yapa-fintech-back.vercel.app" || "http://localhost:4000"; 
+
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const app = express();
-// app.use(cors({
-//   origin: 'http://localhost:5173', 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json());
-app.use('/api', routes);
-// Documentación Swagger
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-  swaggerOptions: { url: '/api/docs/swagger.json' }
-}));
 
-app.get('/api/docs/swagger.json', (req, res) => {
-  res.json(swaggerSpec);
-});
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Yapa Documentación",
+      version: "1.0.0",
+      description: "Documentación de Yapa - Inversiones en Node.js",
+    },
+    servers: [
+      {
+        url: `${URL_BACK}/api`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  apis: ["./routes/**/*.js"], 
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+app.use("/api", routes);
+
+
+app.use(
+  "/api/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerSpec, { customCssUrl: CSS_URL })
+);
 
 (async () => {
   try {
     await conn.sync({ force: false });
-    console.log('Base de datos sincronizada');
+    console.log("Base de datos sincronizada");
   } catch (error) {
-    console.log('Error sincronizando la base de datos:', error);
+    console.log("Error sincronizando la base de datos:", error);
   }
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+  });
 })();
-
